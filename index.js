9895c6c5747e845fc8235917f6edbebc85cd28ea
NeoMashego <mashegonoj@gmail.com>
2025-01-02 15:50:30 +0200

    startButton given functionality for testing

---
+++ index.js
@@ -0,0 +1,10 @@
+let startButton = document.getElementById('startButton')
+
+startButton.onclick = function(){
+    //window.location.href = 
+    openBeef();
+}
+
+function openBeef(){
+    alert("Beef Recipe loading")
+}
