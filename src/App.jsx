d12fb4b9aaf12bcc30a33ca55af0aeb6047e0afc
NeoMashego <mashegonoj@gmail.com>
2025-02-26 09:47:10 +0200

    set up app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
-
+import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom"
+import './App.css'
 
 function App() {
+    <BrowserRouter>
+    <nav>
+        <NavLink to="/" className={({isActive}) => isActive ? "active-link" : null} >Logo</NavLink>
+        <NavLink to="/menu" className={({isActive}) => isActive ? "active-link" : null} >Menu</NavLink>
+        <NavLink to="/contact" className={({isActive}) => isActive ? "active-link" : null} >Contact</NavLink>
+    </nav>
+    <Routes>
+        <Route />
+    </Routes>
+    </BrowserRouter>
 }
 
 export default App
