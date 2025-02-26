c8f454c7f4239b1b8693c396cd47f54ca58393e4
NeoMashego <mashegonoj@gmail.com>
2025-02-26 09:57:43 +0200

    return value added to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom"
 import './App.css'
 
 function App() {
-    <BrowserRouter>
-    <nav>
-        <NavLink to="/" className={({isActive}) => isActive ? "active-link" : null} >Logo</NavLink>
-        <NavLink to="/menu" className={({isActive}) => isActive ? "active-link" : null} >Menu</NavLink>
-        <NavLink to="/contact" className={({isActive}) => isActive ? "active-link" : null} >Contact</NavLink>
-    </nav>
-    <Routes>
-        <Route />
-    </Routes>
-    </BrowserRouter>
+    return(
+        <BrowserRouter>
+        <nav>
+            <NavLink to="/" className={({isActive}) => isActive ? "active-link" : null} >Logo</NavLink>
+            <NavLink to="/menu" className={({isActive}) => isActive ? "active-link" : null} >Menu</NavLink>
+            <NavLink to="/contact" className={({isActive}) => isActive ? "active-link" : null} >Contact</NavLink>
+        </nav>
+        <Routes>
+            <Route />
+        </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
